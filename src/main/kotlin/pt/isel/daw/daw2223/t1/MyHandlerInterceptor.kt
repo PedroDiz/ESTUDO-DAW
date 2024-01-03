ef4ac769087dcbc0225772f9c69f879ae0e278ab
PedroDiz <A47718@alunos.isel.pt>
2024-01-03 16:15:02 +0000

    desenvolvimento

--- src/main/kotlin/pt/isel/daw/daw2223/t1/MyHandlerInterceptor.kt
+++ src/main/kotlin/pt/isel/daw/daw2223/t1/MyHandlerInterceptor.kt
@@ -2,6 +2,7 @@ package pt.isel.daw.daw2223.t1
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
@@ -11,8 +12,11 @@ class MyHandlerInterceptor(
     private val handlersStorage : HandlerStorage
 ) : HandlerInterceptor {
 
-    override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+    private val logger = LoggerFactory.getLogger(MyHandlerInterceptor::class.java)
+
+    override fun preHandle( request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val handlerMethod = handler as HandlerMethod
+        logger.info("Handler method ${handlerMethod.shortLogMessage}")
         if(handlerMethod.methodParameters.firstOrNull { it.parameterName == "id" } == null && !request.requestURI.startsWith("/handler/")) {
             return true
         }
---
+++ src/main/kotlin/pt/isel/daw/daw2223/t1/desenvolvimento.md
@@ -0,0 +1,7 @@
+## Resposta 2
+
+- A anotacao @Component e usada para o spring context saber que deve criar uma instancia da classe anotada e consequentemente injeta-la numa classe que a use. Isto e injecao de dependencias.
+
+## Resposta 3
+
+- A razao de se usar o webpack e na poupanca de pedidos que o browser tem de fazer ao servidor para que consiga ir buscar todos os modulos. O webpack serve para agrupar todos os modulos num ficheiro.
